a3c68476f6e6dde750b6790e42b15413d913043e
Dimitar Markov <dmarkov23@gmail.com>
2017-03-08 15:02:09 +0100

    udating the GUI and make usage of backend logic of abstract factory pattern

--- AbstractFactoryPattern/Client/Form1.Designer.cs
+++ AbstractFactoryPattern/Client/Form1.Designer.cs
@@ -28,20 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.logListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBoatRacing = new System.Windows.Forms.Button();
+            this.btnCarRacing = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Coral;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(76, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(649, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to the greatest game with boats and cars";
+            // 
+            // logListBox
+            // 
+            this.logListBox.FormattingEnabled = true;
+            this.logListBox.ItemHeight = 16;
+            this.logListBox.Location = new System.Drawing.Point(82, 226);
+            this.logListBox.Name = "logListBox";
+            this.logListBox.Size = new System.Drawing.Size(651, 292);
+            this.logListBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(78, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Log: ";
+            // 
+            // btnBoatRacing
+            // 
+            this.btnBoatRacing.Location = new System.Drawing.Point(82, 113);
+            this.btnBoatRacing.Name = "btnBoatRacing";
+            this.btnBoatRacing.Size = new System.Drawing.Size(170, 41);
+            this.btnBoatRacing.TabIndex = 3;
+            this.btnBoatRacing.Text = "Start boat racing game";
+            this.btnBoatRacing.UseVisualStyleBackColor = true;
+            this.btnBoatRacing.Click += new System.EventHandler(this.btnBoatRacing_Click);
+            // 
+            // btnCarRacing
+            // 
+            this.btnCarRacing.Location = new System.Drawing.Point(289, 113);
+            this.btnCarRacing.Name = "btnCarRacing";
+            this.btnCarRacing.Size = new System.Drawing.Size(157, 42);
+            this.btnCarRacing.TabIndex = 4;
+            this.btnCarRacing.Text = "Start car racing game";
+            this.btnCarRacing.UseVisualStyleBackColor = true;
+            this.btnCarRacing.Click += new System.EventHandler(this.btnCarRacing_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 341);
+            this.ClientSize = new System.Drawing.Size(819, 545);
+            this.Controls.Add(this.btnCarRacing);
+            this.Controls.Add(this.btnBoatRacing);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.logListBox);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox logListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBoatRacing;
+        private System.Windows.Forms.Button btnCarRacing;
     }
 }
 
--- AbstractFactoryPattern/Client/Form1.cs
+++ AbstractFactoryPattern/Client/Form1.cs
@@ -1,20 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RacingGame;
+using System;
 using System.Windows.Forms;
-
+using FastVehicles;
+using SlowVehicles;
 namespace Client
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+        IFactory f;
+        private void btnBoatRacing_Click(object sender, EventArgs e)
+        {
+            // Based on the button click the factory becomes a boat factory
+            f = new RaceboatFactory();
+
+            // Generating objects from the factory
+            IFastVehicle fastBoatVehicle = f.CreateFastRacingVehicle();
+            ISlowVehicle slowBoatVehicle = f.CreateSlowRacingVehicle();
+
+            // Updating listbox
+            logListBox.Items.Add("-------A very dynamic race with boats started-------");
+            logListBox.Items.Add("");
+
+            // Making use of the fast vehicle by calling the methods and directly adding them to
+            // the list box
+            logListBox.Items.Add(fastBoatVehicle.MoveFast());
+            logListBox.Items.Add(fastBoatVehicle.GainSpeed());
+
+            // Making use of the slow vehicle
+            logListBox.Items.Add(slowBoatVehicle.DodgeObstacles());
+            logListBox.Items.Add(slowBoatVehicle.MoveSlowly());
+        }
+
+        private void btnCarRacing_Click(object sender, EventArgs e)
+        {
+            // Based on the button click the factory becomes a car factory
+            f = new RacecarFactory();
+
+            // Generating objects from the factory
+            IFastVehicle fastCarVehicle = f.CreateFastRacingVehicle();
+            ISlowVehicle slowCarVehicle = f.CreateSlowRacingVehicle();
+
+            // Updating listbox
+            logListBox.Items.Add("-------A very dynamic race with cars started-------");
+            logListBox.Items.Add("");
+
+            // Making use of the fast vehicle by calling the methods and directly adding them to
+            // the list box
+            logListBox.Items.Add(fastCarVehicle.MoveFast());
+            logListBox.Items.Add(fastCarVehicle.GainSpeed());
+
+            // Making use of the slow vehicle
+            logListBox.Items.Add(slowCarVehicle.DodgeObstacles());
+            logListBox.Items.Add(slowCarVehicle.MoveSlowly());
         }
     }
 }
