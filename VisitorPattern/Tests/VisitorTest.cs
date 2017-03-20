ebad90f0a3145069a3e171bf29b3ffa67e81dac5
Dimitar Markov <dmarkov23@gmail.com>
2017-03-20 05:14:34 +0100

    testing visitor pattern

---
+++ VisitorPattern/Tests/VisitorTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VisitorPattern;
+namespace Tests
+{
+    [TestClass]
+    public class VisitorTest
+    {
+        [TestMethod]
+        public void Test_Oven_Visiting_Chicken()
+        {
+            Chicken chicken = new Chicken();
+
+            OvenVisitor ovenVisitor = new OvenVisitor();
+            string result = ovenVisitor.Visit(chicken);
+
+            Assert.AreEqual(result, "The CHICKEN meat is being prepared in the OVEN");
+        }
+        [TestMethod]
+        public void Test_Oven_Visiting_Beef()
+        {
+            Beef  beef = new Beef();
+
+            OvenVisitor ovenVisitor = new OvenVisitor();
+            string result = ovenVisitor.Visit(beef);
+
+            Assert.AreEqual(result, "The BEEF meat is being prepared in the OVEN");
+        }
+        [TestMethod]
+        public void Test_Pan_Visiting_Chicken()
+        {
+            Chicken chicken = new Chicken();
+
+            PanVisitor panVisitor = new PanVisitor();
+            string result = panVisitor.Visit(chicken);
+
+            Assert.AreEqual(result, "The CHICKEN meat is being prepared in the PAN");
+        }
+        [TestMethod]
+        public void Test_Pan_Visiting_Beef()
+        {
+            Beef beef = new Beef();
+
+            PanVisitor panVisitor = new PanVisitor();
+            string result = panVisitor.Visit(beef);
+
+            Assert.AreEqual(result, "The BEEF meat is being prepared in the PAN");
+        }
+    }
+}
